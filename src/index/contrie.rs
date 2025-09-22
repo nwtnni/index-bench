c30ec3149ea2b36fb86a1db1029aa980234569fd
Newton Ni <nwtnni@gmail.com>
2025-09-22 16:21:10 -0500

    Switch to `CloneConMap`
    Since values are currently hard-coded to u32, we don't need the `Arc`.

--- src/index/contrie.rs
+++ src/index/contrie.rs
@@ -1,13 +1,13 @@
 use crate::Index;
 use crate::index;
 
-pub struct Map<K: index::Key>(contrie::ConMap<K, u32>);
+pub struct Map<K: index::Key>(contrie::CloneConMap<K, u32>);
 
 impl<K: index::Key, H: index::Hasher> Index<K, H> for Map<K> {
     type Handle = Self;
 
     fn new() -> Self {
-        Self(contrie::ConMap::new())
+        Self(contrie::CloneConMap::new())
     }
 
     fn pin(&self) -> Self::Handle {
@@ -17,11 +17,11 @@ impl<K: index::Key, H: index::Hasher> Index<K, H> for Map<K> {
 
 impl<K: index::Key> index::Handle<K> for Map<K> {
     fn get(&mut self, key: &K) -> Option<u32> {
-        self.0.get(key).map(|element| *element.value())
+        self.0.get(key).map(|(_, value)| value)
     }
 
     fn insert(&mut self, key: K, value: u32) -> Option<u32> {
-        self.0.insert(key, value).map(|element| *element.value())
+        self.0.insert(key, value).map(|(_, value)| value)
     }
 
     fn scan(&mut self, _key: &K, _count: usize) -> impl Iterator<Item = u32> {
