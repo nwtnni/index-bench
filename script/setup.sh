981b46f3efafb40c5b95dcfd51efaabceb7afb10
Newton Ni <nwtnni@gmail.com>
2025-11-17 11:25:07 -0600

    Make setup script idempotent

--- script/setup.sh
+++ script/setup.sh
@@ -7,23 +7,39 @@ set -o xtrace
 
 cd ~
 
-bash <(curl -L https://nixos.org/nix/install) --no-daemon
+sudo apt update
+sudp apt install ripgrep
 
-curl -sfL https://direnv.net/install.sh | sudo -E bin_path=/usr/bin bash
+if command -v nix &>/dev/null; then
+    echo "Skipping nix installation"
+else
+    bash <(curl -L https://nixos.org/nix/install) --no-daemon
+fi
 
-echo 'eval "$(direnv hook bash)"' >> ~/.bashrc
+if command -v direnv &>/dev/null; then
+    echo "Skipping direnv installation"
+else
+    curl -sfL https://direnv.net/install.sh | sudo -E bin_path=/usr/bin bash
+fi
 
-# git clone git@github.com:nwtnni/.dot.git
-# nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
-# nix-channel --update
-# nix-shell '<home-manager>' -A install
-# home-manager init --switch .
+rg -q direnv ~/.bashrc || echo 'eval "$(direnv hook bash)"' >> ~/.bashrc
 
-git clone git@github.com:nwtnni/index-bench.git
-cd index-bench
+if command -v home-manager &>/dev/null; then
+    echo "Skipping home-manager installation"
+else
+    nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+    nix-channel --update
+    nix-shell '<home-manager>' -A install
+fi
+
+[ -d "~/.dot" ] || git clone git@github.com:nwtnni/.dot.git
+cd ~/.dot
+sed -i"" "s/local = true/local = false/" home.nix
+home-manager init --switch . -b bak
+
+[ -d "~/index-bench" ] || git clone git@github.com:nwtnni/index-bench.git
+cd ~/index-bench
 git submodule update --init --recursive
-echo "use flake" > .envrc
+rg -q "flake" .envrc || echo 'use flake' >> .envrc
 direnv allow .
-
-cd ~
-./index-bench/scripts/normalize.sh
+./script/normalize.sh
