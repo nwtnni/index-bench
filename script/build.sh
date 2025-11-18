49b243c83d0eda76ee2ba293ed078ca0d4f60b70
Newton Ni <nwtnni@gmail.com>
2025-11-18 12:12:01 -0600

    Add script to build mimalloc

---
+++ script/build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# https://stackoverflow.com/a/246128
+ROOT=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o xtrace
+
+git submodule update --init --recursive
+
+cd "$ROOT/../extern/mimalloc_rust/libmimalloc-sys/c_src/mimalloc/v3/"
+mkdir build && cd build
+cmake ..
+make -j mimalloc-static
+
