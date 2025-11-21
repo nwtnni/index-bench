b9b7a86b3966d04d9c6f957a7108753b51ba75ee
Newton Ni <nwtnni@gmail.com>
2025-11-21 09:06:35 -0600

    Support string keys for papaya

--- src/index/papaya.rs
+++ src/index/papaya.rs
@@ -1,28 +1,34 @@
 use crate::Index;
 use crate::index;
 
-impl<H: index::Hasher> Index<u64, H> for papaya::HashMap<u64, u64, H> {
-    type Send<'a> = &'a Self;
+macro_rules! impl_index {
+    ($index:ty, $map:ty) => {
+        impl<H: index::Hasher> Index<$index, H> for papaya::HashMap<$map, u64, H> {
+            type Send<'a> = &'a Self;
 
-    fn new(_: &index::Config) -> Self {
-        papaya::HashMap::with_hasher(H::default())
-    }
+            fn new(_: &index::Config) -> Self {
+                papaya::HashMap::with_hasher(H::default())
+            }
 
-    fn send<'a>(&'a self) -> Self::Send<'a> {
-        self
-    }
+            fn send<'a>(&'a self) -> Self::Send<'a> {
+                self
+            }
+        }
+
+        impl<H: index::Hasher> index::IndexSend<$index, H> for &'_ papaya::HashMap<$map, u64, H> {
+            type Handle<'a>
+                = Self
+            where
+                Self: 'a;
+
+            fn pin<'a>(&'a self) -> Self::Handle<'a> {
+                self
+            }
+        }
+    };
 }
 
-impl<H: index::Hasher> index::IndexSend<u64, H> for &'_ papaya::HashMap<u64, u64, H> {
-    type Handle<'a>
-        = Self
-    where
-        Self: 'a;
-
-    fn pin<'a>(&'a self) -> Self::Handle<'a> {
-        self
-    }
-}
+impl_index!(u64, u64);
 
 impl<H: index::Hasher> index::IndexPin<u64> for &'_ papaya::HashMap<u64, u64, H> {
     fn get(&mut self, key: u64) -> Option<u64> {
@@ -46,28 +52,7 @@ impl<H: index::Hasher> index::IndexPin<u64> for &'_ papaya::HashMap<u64, u64, H>
     }
 }
 
-impl<H: index::Hasher> Index<String, H> for papaya::HashMap<&'static str, u64, H> {
-    type Send<'a> = &'a Self;
-
-    fn new(_: &index::Config) -> Self {
-        papaya::HashMap::with_hasher(H::default())
-    }
-
-    fn send<'a>(&'a self) -> Self::Send<'a> {
-        self
-    }
-}
-
-impl<H: index::Hasher> index::IndexSend<String, H> for &'_ papaya::HashMap<&'static str, u64, H> {
-    type Handle<'a>
-        = Self
-    where
-        Self: 'a;
-
-    fn pin<'a>(&'a self) -> Self::Handle<'a> {
-        self
-    }
-}
+impl_index!(String, &'static str);
 
 impl<H: index::Hasher> index::IndexPin<String> for &'_ papaya::HashMap<&'static str, u64, H> {
     fn get(&mut self, key: &'static str) -> Option<u64> {
@@ -88,5 +73,29 @@ impl<H: index::Hasher> index::IndexPin<String> for &'_ papaya::HashMap<&'static 
     fn remove(&mut self, key: &'static str) -> Option<u64> {
         let map = self.pin();
         map.remove(&key).copied()
+    }
+}
+
+impl_index!(String, String);
+
+impl<H: index::Hasher> index::IndexPin<String> for &'_ papaya::HashMap<String, u64, H> {
+    fn get(&mut self, key: &'static str) -> Option<u64> {
+        let map = self.pin();
+        map.get(key).copied()
+    }
+
+    fn insert(&mut self, key: &'static str, value: u64) -> Option<u64> {
+        let map = self.pin();
+        map.insert(key.to_owned(), value).copied()
+    }
+
+    fn update(&mut self, key: &'static str, value: u64) -> Option<u64> {
+        let map = self.pin();
+        map.update(key.to_owned(), |_| value).copied()
+    }
+
+    fn remove(&mut self, key: &'static str) -> Option<u64> {
+        let map = self.pin();
+        map.remove(key).copied()
     }
 }
