45628e083e08c8f58dc3a652ce9a9c094264f07f
Newton Ni <nwtnni@gmail.com>
2026-01-15 14:30:46 -0600

    Download datasets

--- script/setup.sh
+++ script/setup.sh
@@ -25,21 +25,29 @@ fi
 
 rg -q direnv ~/.bashrc || echo 'eval "$(direnv hook bash)"' >> ~/.bashrc
 
-if command -v home-manager &>/dev/null; then
-    echo "Skipping home-manager installation"
-else
-    nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
-    nix-channel --update
-    nix-shell '<home-manager>' -A install
-fi
-
-[ -d ".dot" ] || git clone git@github.com:nwtnni/.dot.git
-cd ~/.dot
-sed -i"" "s/local = true/local = false/" home.nix
-home-manager init --switch . -b bak
+# NOTE: below is for setting up dotfiles
+# if command -v home-manager &>/dev/null; then
+#     echo "Skipping home-manager installation"
+# else
+#     nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+#     nix-channel --update
+#     nix-shell '<home-manager>' -A install
+# fi
+#
+# [ -d ".dot" ] || git clone git@github.com:nwtnni/.dot.git
+# cd ~/.dot
+# sed -i"" "s/local = true/local = false/" home.nix
+# home-manager init --switch . -b bak
 
 cd ~
 [ -d "index-bench" ] || git clone git@github.com:nwtnni/index-bench.git
+
+cd ~/index-bench/data
+[ -f 'email.txt' ] || wget -O email.txt.gz 'https://www.dropbox.com/scl/fi/fif8lg9vwosftb3hyew61/email.txt.gz?rlkey=5649fx3b4ae8mnqg6e7ts6rrl&st=hpbbt6hx&dl=1' && gunzip email.txt.gz
+[ -f 'ipv4.bin' ] || wget -O ipv4.bin.gz 'https://www.dropbox.com/scl/fi/0lbdg8d4dx1xj81e7v4yp/ipv4.bin.gz?rlkey=ezt27zcppxbvuh4xdwl4jmy4m&st=i7ohi54p&dl=1' && gunzip ipv4.bin.gz
+[ -f 'snowflake.bin' ] || wget -O snowflake.bin.gz 'https://www.dropbox.com/scl/fi/q9vnnitkxj8cu71eer3nj/snowflake.bin.gz?rlkey=zi5f62591w30qbbpl76pk6fc6&st=wk02kan3&dl=1' && gunzip snowflake.bin.gz
+[ -f 'url.txt' ] || wget -O url.txt.gz 'https://www.dropbox.com/scl/fi/eurnrj268bdxhbjjzmq7z/url.txt.gz?rlkey=9upb8mmygnjvjgbtlnwya9y73&st=ssm465fb&dl=1' && gunzip url.txt.gz
+
 cd ~/index-bench
 git submodule update --init --recursive
 rg -q "flake" .envrc || echo 'use flake' >> .envrc
