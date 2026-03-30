dccef4d20dd2e1c64bdf9048cc4b18bed61394b1
Michael Goppert <mi@gopp.dev>
2026-03-30 18:07:07 +0000

    Revert "Fix mimalloc stats FFI call UB"
    This reverts commit 8ac0f8843acab95fdb60245d73498eecf522c468.

--- src/measure/mimalloc.rs
+++ src/measure/mimalloc.rs
@@ -4,8 +4,7 @@ use serde::Deserialize;
 use serde::Serialize;
 
 unsafe extern "C" {
-    // https://github.com/migopp/mimalloc/blob/dev3/src/stats.c#L796
-    fn mi_stats_get_json(buf_size: usize, buf: *mut ffi::c_char) -> *mut ffi::c_char;
+    fn mi_stats_get_json() -> *mut ffi::c_char;
 }
 
 // https://github.com/microsoft/mimalloc/blob/09a27098aa6e9286518bd9c74e6ffa7199c3f04e/include/mimalloc-stats.h
@@ -22,9 +21,7 @@ pub struct Mimalloc {
 impl Mimalloc {
     #[expect(clippy::new_without_default)]
     pub fn new() -> Self {
-        // Allocates buffer if `buf` is null.
-        // https://github.com/migopp/mimalloc/blob/dev3/src/stats.c#L717
-        let json = unsafe { ffi::CString::from_raw(mi_stats_get_json(0, std::ptr::null_mut())) };
+        let json = unsafe { ffi::CString::from_raw(mi_stats_get_json()) };
         let json = json.to_string_lossy();
         serde_json::from_str(&json).expect("Expected valid JSON from mimalloc")
     }
