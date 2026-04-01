ccff2d790a70d25c2ed8f73e4494d16ed8573ee3
Michael Goppert <mi@gopp.dev>
2026-04-01 20:26:07 +0000

    Update workloads and scripts

--- script/smr-perf-test.sh
+++ script/smr-perf-test.sh
@@ -6,14 +6,14 @@ set -o nounset
 set -o xtrace
 
 # Once with membarrier.
-mkdir -p raw/reclaim-test/membarrier/perf/folded
-for smr in "default" "smr-seize" "smr-epoch" "smr-disable"; do
-    cargo build --release --features "membarrier,$smr"
-    ./perf-record.sh cargo run --release  --features "membarrier,$smr" -- bench/smr-box-single.toml
-    mv out.folded raw/reclaim-test/membarrier/perf/folded/"arctic-reclaim-$smr.folded"
-    mv out.svg raw/reclaim-test/membarrier/perf/"arctic-reclaim-$smr.svg"
-done
-
+# mkdir -p raw/reclaim-test/membarrier/perf/folded
+# for smr in "default" "smr-seize" "smr-epoch" "smr-disable"; do
+#     cargo build --release --features "membarrier,$smr"
+#     ./perf-record.sh cargo run --release  --features "membarrier,$smr" -- bench/smr-box-single.toml
+#     mv out.folded raw/reclaim-test/membarrier/perf/folded/"arctic-reclaim-$smr.folded"
+#     mv out.svg raw/reclaim-test/membarrier/perf/"arctic-reclaim-$smr.svg"
+# done
+#
 # And once without...
 mkdir -p raw/reclaim-test/no-membarrier/perf/folded
 for smr in "default" "smr-seize" "smr-epoch" "smr-disable"; do
@@ -26,12 +26,12 @@ done
 # Now get the differential flamegraphs.
 #
 # https://docs.rs/inferno/0.12.6/inferno/#differential-flame-graphs
-mkdir -p raw/reclaim-test/perf-cmp/folded
-for smr in "default" "smr-seize"; do
-    # Use the folds we saved to gen differential fold.
-    ~/.cargo/bin/inferno-diff-folded raw/reclaim-test/membarrier/perf/folded/"arctic-reclaim-$smr.folded" raw/reclaim-test/no-membarrier/perf/folded/"arctic-reclaim-$smr.folded" > raw/reclaim-test/perf-cmp/folded/"arctic-reclaim-$smr.folded"
-
-    # Get both diffs in svg.
-    cat raw/reclaim-test/perf-cmp/folded/"arctic-reclaim-$smr.folded" | ~/.cargo/bin/inferno-flamegraph raw/reclaim-test/perf-cmp/"arctic-reclaim-$smr.folded" >  raw/reclaim-test/perf-cmp/"arctic-reclaim-$smr-diff1.svg"
-    cat raw/reclaim-test/perf-cmp/folded/"arctic-reclaim-$smr.folded" | ~/.cargo/bin/inferno-flamegraph raw/reclaim-test/perf-cmp/"arctic-reclaim-$smr.folded" --negate >  raw/reclaim-test/perf-cmp/"arctic-reclaim-$smr-diff2.svg"
-done
+# mkdir -p raw/reclaim-test/perf-cmp/folded
+# for smr in "default" "smr-seize"; do
+#     # Use the folds we saved to gen differential fold.
+#     ~/.cargo/bin/inferno-diff-folded raw/reclaim-test/membarrier/perf/folded/"arctic-reclaim-$smr.folded" raw/reclaim-test/no-membarrier/perf/folded/"arctic-reclaim-$smr.folded" > raw/reclaim-test/perf-cmp/folded/"arctic-reclaim-$smr.folded"
+#
+#     # Get both diffs in svg.
+#     cat raw/reclaim-test/perf-cmp/folded/"arctic-reclaim-$smr.folded" | ~/.cargo/bin/inferno-flamegraph raw/reclaim-test/perf-cmp/"arctic-reclaim-$smr.folded" >  raw/reclaim-test/perf-cmp/"arctic-reclaim-$smr-diff1.svg"
+#     cat raw/reclaim-test/perf-cmp/folded/"arctic-reclaim-$smr.folded" | ~/.cargo/bin/inferno-flamegraph raw/reclaim-test/perf-cmp/"arctic-reclaim-$smr.folded" --negate >  raw/reclaim-test/perf-cmp/"arctic-reclaim-$smr-diff2.svg"
+# done
